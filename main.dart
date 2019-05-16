77098124821027bf65c7fcfb37d3b8b2e9a2ecf5
maplbop135 <48009213+maplbop135@users.noreply.github.com>
2019-05-16 10:22:43 +0900

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+/*
+*
+* 21500344 서찬솔, 21600062 김미소
+* 2019 Mobile Application Development
+* Project: Moamoa
+*
+* Moamoa is an application that shows recipies from some ingredients.
+* Users can enroll their own ingredients, and get recipies of food
+* which they can make.
+*
+* v0.8
+* app.dart: 페이지 목록 관리.
+* home.dart: 냉장고 생성 및 관리.
+* ingredients.dart:12개의 재료 생성.
+* login.dart: 로그인 페이지(구글, 익명).
+* mypage.dart: 로그인 정보.
+* recipies.dart: 6개의 레시피 생성.
+*
+*/
+
+import 'package:flutter/material.dart';
+import 'package:moamoa/app.dart';
